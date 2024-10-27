fb6a9dd50fecd20f5dc3bbadf7636fe8e8d70a07
OMGPLEASEEEEEE <0lofiwifi0@gmail.com>
2024-10-26 20:02:07 -0500

    Add files via upload

---
+++ PLEASE.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Scene-Based Music Player</title>
+  <script src="https://raw.githubusercontent.com/OMGPLEASEEEEEE/Music/refs/heads/master/music.js"></script>
+</head>
+<body>
+  <audio id="audioPlayer" src="song1.mp3"></audio>
+  <button onclick="playScene(1)">Scene 1</button><br>
+  <button onclick="playScene(2)">Scene 2</button><br>
+  <button onclick="playScene(3)">Scene 3</button><br><br>
+  <p>Current Scene: <span id="currentScene"></span></p>
+</body>
+</html>
+}
+
